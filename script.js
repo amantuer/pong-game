8f68af435089ade567316dde19defeee636f8371
tuerfight <tuerfight@users.noreply.github.com>
2022-05-01 22:41:53 +0900

    add pause

--- script.js
+++ script.js
@@ -13,33 +13,86 @@ const HARD_M = 0.06
 let SPEED = 0.02
 
 let lastTime
+let isPaused = false
+let stateStore = {
+  delta : 0,
+  lastTime : 0,
+  paddles : 0 ,
+  playerScoreElem : {
+    textContent: 0,
+  },
+  computerScoreElem : {
+    textContent : 0,
+  },
+  playerPaddle : {
+    position : 0
+  },
+  computerPaddle : {
+    position : 0,
+  },
+  ball : {
+     x : 0,
+     y : 0,
+     direction : 0,
+     velocity : 0,
+  }
+}
 //passing time argument as how much time passed since program start
 function update(time) {
   if(lastTime != null) {
-   const delta = time - lastTime
-   ball.update(delta,[playerPaddle.rect(),computerPaddle.rect()])
-   computerPaddle.update(delta, ball.y, SPEED)
-   const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--hue'))
+   const delta = time - stateStore.lastTime
+   // after pause recover: delta = 0.000000001 * time // or something like that 
+   stateStore.delta = delta
+   stateStore.paddles = [playerPaddle.rect(),computerPaddle.rect()]
+   
+   if(!isPaused){
+     ball.update(stateStore.delta, stateStore.paddles)
+   }else{
+     // set point and ball paddles position
+     // score
+     playerScoreElem.textContent = stateStore.playerScoreElem.textContent
+     computerScoreElem.textContent = stateStore.computerScoreElem.textContent
+     // paddle
+     playerPaddle.position = stateStore.playerPaddle.position 
+     computerPaddle.position = stateStore.computerPaddle.position
+     //ball
+     ball.x = stateStore.ball.x
+     ball.y = stateStore.ball.y
+     ball.direction = stateStore.ball.direction
+     ball.velocity = stateStore.ball.velocity
+   }
 
+   computerPaddle.update(delta, ball.y, SPEED)
+   stateStore.computerPaddle.position = computerPaddle.position
+
+   const hue = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--hue'))
    document.documentElement.style.setProperty('--hue', hue + delta * .01)
 
-   if(isLose()) handleLose()
+   if(isLose()) handleLoseAndPause(isPaused)
   }
   lastTime = time
+  stateStore.lastTime = lastTime
+
   window.requestAnimationFrame(update)
 }
 
 function isLose(){
    const rect = ball.rect()
+   stateStore.ball.x = ball.x
+   stateStore.ball.y = ball.y
+   stateStore.ball.direction = ball.direction
+   stateStore.ball.velocity = ball.velocity
    return rect.right >= window.innerWidth || rect.left <= 0
 }
 
-function handleLose() {
+function handleLoseAndPause() {
   const rect = ball.rect()
   if(rect.right >= window.innerWidth){
-    playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+    playerScoreElem.textContent = parseInt(stateStore.playerScoreElem.textContent) + 1
+    stateStore.playerScoreElem.textContent = playerScoreElem.textContent
   }else {
-    computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+    computerScoreElem.textContent = parseInt(stateStore.computerScoreElem.textContent) + 1
+    stateStore.computerScoreElem.textContent = computerScoreElem.textContent
   }
   ball.reset()
   computerPaddle.reset()
@@ -47,12 +100,41 @@ function handleLose() {
 
 // convert pixels value to vh get result of 0 ~ 100
 document.addEventListener('mousemove', e => {
-  playerPaddle.position = (e.y / window.innerHeight) * 100
+  if(!isPaused)playerPaddle.position = (e.y / window.innerHeight) * 100
+  if(!isPaused)stateStore.playerPaddle.position = playerPaddle.position
+})
+document.addEventListener('touchmove', e => {
+  if(!isPaused)playerPaddle.position = (e.y / window.innerHeight) * 100
+  if(!isPaused)stateStore.playerPaddle.position =  playerPaddle.position
+})
+
+// add event listener to handle game pause
+document.getElementById('pause').addEventListener('click', e => {
+   isPaused = !isPaused 
+})
+document.getElementById('pause').addEventListener('touchstart', e => {
+   isPaused = !isPaused 
 })
 
 // add event listener to speed change button
 const buttons = document.getElementsByClassName('Normal');
 Array.from(buttons).forEach(button => button.addEventListener('click', e => {   
+    Array.from(buttons).forEach((button) => {
+       if(button.classList.contains('active')){
+         button.classList.remove('active')       
+       }
+    }) 
+    e.target.classList.add('active')
+    switch(e.target.id){
+      case 'easy':  SPEED = EASY_M;
+        break;
+      case 'normal': SPEED = NORMAL_M;    
+        break;
+      case 'hard': SPEED = HARD_M;  
+        break;  
+    }
+}));
+Array.from(buttons).forEach(button => button.addEventListener('touchstart', e => {   
     Array.from(buttons).forEach((button) => {
        if(button.classList.contains('active')){
          button.classList.remove('active')       
