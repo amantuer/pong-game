23489fdc6766ef7ad715cc4b31f4a6914d643d4a
tuerfight <tuerfight@users.noreply.github.com>
2022-05-15 18:58:14 +0900

    make code beautiful and perpaer for being public reportary

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import Ball from "./Ball.js";
 import Paddle from './Paddle.js'
-// get the ball element in html and create a instance of class for that ball
+
 const ball = new Ball(document.getElementById('ball'))
 const playerPaddle = new Paddle(document.getElementById('player-paddle'))
 const computerPaddle = new Paddle(document.getElementById('computer-paddle'))
@@ -138,8 +138,7 @@ function handlePause(e){
 pauseButton.addEventListener('click', handlePause)
 pauseButton.addEventListener('touchstart', handlePause)
 
-// We need to add touchend handle on 
-// We don't need hover event on mobile read mdn read code change css when touchstirt
+
 const restartButton = document.getElementById('restart');
 function handleRestart(e){
    if(e.type === 'touchstart' || e.type === 'touchmove' || e.type === 'touchend' || e.type === 'touchcancel'){
@@ -159,7 +158,7 @@ function handleRestart(e){
 restartButton.addEventListener('click', handleRestart)
 restartButton.addEventListener('touchend', handleRestart)
 
-// add event listener to speed change butto
+
 const buttons = document.getElementsByClassName('Normal');
 function handleHardMenu(e){
    if(e.type === 'touchstart' || e.type === 'touchmove' || e.type === 'touchend' || e.type === 'touchcancel'){
@@ -194,16 +193,3 @@ Array.from(buttons).forEach(button => button.addEventListener('touchend', handle
 
 window.requestAnimationFrame(update)
 
-/*
-1. 
-setInterval(,10) 
-This is bad practive. Because setInterval is not super accurate. It's not gonna going to run every 10 milliseconds so you kind of have some problems there, And also it's going to run between frames. With request animation frame what happens is every time that you can change what's on the screen this function is going to be called so js is smart enough to say: you can't change anything on the screen so don't even bother running code but now as soon as you can run something on the screen it calls this function for us.
-And as when keep calling window.requestAnimationFrame it's going to infinitely loop so right here we've created an infinite loop which is called updated every single time that something on our screen is allowed to change so every frame it's going to call this function for us and all we need to do is set this up and now we have that update loop 
-*/
-
-/*
-2.
-   This update function is a function we're going to create(inside Ball class) and we're passing in the delta into this function, the reason we're passing that delta in is because as you saw that delta fluctuated波动 somethimes somethime it was 6.9 millisecond sometimes it wa 7.1 millisecond it fluctuated a little bit. It's important to use that delta to make sure all of our movements in our game are based on that because if for example we have a frame drop and the time between frames is like 50 milliseconds 
-   we want to make sure we take that into account when we're performing our 
-   calculations 
-*/
